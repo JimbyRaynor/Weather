a53e2268cd75510ca47b2fb47d0ce02f842064ee
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2025-10-03 12:29:41 +1000

    Add files via upload

--- weathergui.py
+++ weathergui.py
@@ -36,18 +36,18 @@ class GameObject:
 
 mainwin = Tk()
 mainwin.geometry("1600x900+1+1") # window is at x=1, y=1 on screen (top left)
-canvas1 = Canvas(mainwin,width=1600,height= 900,bg="black")
+canvas1 = Canvas(mainwin,width=1920,height= 1080,bg="black")
 canvas1.place(x=0,y=0)
 
 
 #spritecloud3 = GameObject("Showers.png",x=500,y=30)
-spriteForcast0 = GameObject("sun.png","Showers.png",x=500,y=30)
-spriteForcast1 = GameObject("sunsmall.png","Showerssmall.png",x=350,y=195)
-spriteForcast2 = GameObject("sunsmall.png","Showerssmall.png",x=350,y=295)
-spriteForcast3 = GameObject("sunsmall.png","Showerssmall.png",x=350,y=395)
-spriteForcast4 = GameObject("sunsmall.png","Showerssmall.png",x=350,y=495)
-spriteForcast5 = GameObject("sunsmall.png","Showerssmall.png",x=350,y=595)
-spriteForcast6 = GameObject("sunsmall.png","Showerssmall.png",x=350,y=695)
+spriteForcast0 = GameObject("sun.png","Showers.png","PartlyCloudy.png",x=500,y=30)
+spriteForcast1 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png",x=350,y=195)
+spriteForcast2 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png",x=350,y=295)
+spriteForcast3 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png",x=350,y=395)
+spriteForcast4 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png",x=350,y=495)
+spriteForcast5 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png",x=350,y=595)
+spriteForcast6 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png",x=350,y=695)
 
 fontbig = ("Arial",70)
 fontmedium = ("Arial",45)
@@ -57,7 +57,7 @@ fonttiny2 = ("Arial",18)
 mytemptext = canvas1.create_text(300,40,font=fontbig,text="temp",fill="yellow")
 myraintext = canvas1.create_text(800,110,font=fontsmall,text="rain summary",fill="#4343D3")
 myupdatedtext = canvas1.create_text(1400,850,font=fonttiny2,text="updated",fill="yellow")
-day0summary = canvas1.create_text(1000,50,font=fontsmall,text="summary",fill="#6B6BEE")
+day0summary = canvas1.create_text(600,50,font=fontsmall,text="summary",anchor="w",fill="#6B6BEE")
 day1summary = canvas1.create_text(390,200,font=fontsmall,text="summary1",anchor="w",fill="#7373DD")
 day2summary = canvas1.create_text(390,300,font=fontsmall,text="summary2",anchor="w",fill="#7373DD")
 day3summary = canvas1.create_text(390,400,font=fontsmall,text="summary3",anchor="w",fill="#7373DD")
@@ -75,6 +75,11 @@ for i in range(1,7):
 
 daylist = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 
+def chooseicon(myspriteobject, textinfo):
+     myspriteobject.changeimage(0) # default to sun
+     if "shower" in  textinfo: myspriteobject.changeimage(1)
+     if "partly cloudy" in  textinfo: myspriteobject.changeimage(2)
+
 def timer1():
     t = localtime()
     daynumber = t.tm_wday
@@ -89,32 +94,31 @@ def timer1():
     canvas1.itemconfigure(myupdatedtext,text=f"🕒 Updated: {time}")
     
     (precip, maxtemp, mintemp,summary) = forecasts[0]
-    if "shower" in summary.lower(): spriteForcast0.changeimage(1)
-    #icon = weather.ascii_icon(summary.lower())
+    chooseicon(spriteForcast0, summary.lower())
     canvas1.itemconfigure(day0summary,text=summary+" "+precip+" rain. Max "+maxtemp+"°C"+". Min "+mintemp+"°C")
+    
     (precip, maxtemp,mintemp, summary) = forecasts[1]
-    if "shower" in summary.lower(): spriteForcast1.changeimage(1)
-    icon = weather.ascii_icon(summary.lower())
+    chooseicon(spriteForcast1, summary.lower())
     canvas1.itemconfigure(day1summary,text=summary+" "+precip+" rain. Min "+mintemp+"°C")
+    
     (precip, maxtemp, mintemp, summary) = forecasts[2]
-    if "shower" in summary.lower(): spriteForcast2.changeimage(1)
-    icon = weather.ascii_icon(summary.lower())
+    chooseicon(spriteForcast2, summary.lower())
     canvas1.itemconfigure(day2summary,text=summary+" "+precip+" rain. Min "+mintemp+"°C")
+
     (precip, maxtemp, mintemp, summary) = forecasts[3]
-    if "shower" in summary.lower(): spriteForcast3.changeimage(1)
-    icon = weather.ascii_icon(summary.lower())
+    chooseicon(spriteForcast3, summary.lower())
     canvas1.itemconfigure(day3summary,text=summary+" "+precip+" rain. Min "+mintemp+"°C")
+
     (precip, maxtemp, mintemp, summary) = forecasts[4]
-    if "shower" in summary.lower(): spriteForcast4.changeimage(1)
-    icon = weather.ascii_icon(summary.lower())
+    chooseicon(spriteForcast4, summary.lower())
     canvas1.itemconfigure(day4summary,text=summary+" "+precip+" rain. Min "+mintemp+"°C")
+
     (precip, maxtemp, mintemp, summary) = forecasts[5]
-    if "shower" in summary.lower(): spriteForcast5.changeimage(1)
-    icon = weather.ascii_icon(summary.lower())
+    chooseicon(spriteForcast5, summary.lower())
     canvas1.itemconfigure(day5summary,text=summary+" "+precip+" rain. Min "+mintemp+"°C")
+
     (precip, maxtemp, mintemp, summary) = forecasts[6]
-    if "shower" in summary.lower(): spriteForcast6.changeimage(1)
-    icon = weather.ascii_icon(summary.lower())
+    chooseicon(spriteForcast6, summary.lower())
     canvas1.itemconfigure(day6summary,text=summary+" "+precip+" rain. Min "+mintemp+"°C")
     mainwin.after(60000,timer1)
 
