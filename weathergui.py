b0035d6d396e64b8b8c0c184218fdb70aa99a2a9
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2025-10-07 09:48:05 +1100

    Add files via upload

--- weathergui.py
+++ weathergui.py
@@ -11,32 +11,20 @@ os.chdir(current_script_directory)
 
 
 class GameObject:
-    def __init__(self,file1="",file2="",file3="",file4="",x=0,y=0):
+    def __init__(self,filelist,x=0,y=0):
         self.x = x
         self.y = y
-        self.image = PhotoImage(file=file1)
-        self.sprite = canvas1.create_image(2,2,image=self.image)
-        if file2 != "":
-           self.image2 = PhotoImage(file=file2)
-        if file3 != "":
-           self.image3 = PhotoImage(file=file3)
-        if file4 != "":
-           self.image4 = PhotoImage(file=file4)
+        self.imagelist = []
+        for myfile in filelist:
+            self.imagelist.append(PhotoImage(file=myfile))
+        self.sprite = canvas1.create_image(2,2,image=self.imagelist[0])
         canvas1.move(self.sprite,x,y)
     def move(self,dx=0,dy=0):
         self.x = self.x + dx
         self.y = self.y + dy
         canvas1.move(self.sprite,dx,dy)     
     def changeimage(self,imagenum):
-         if imagenum == 0:
-           canvas1.itemconfigure(self.sprite,image=self.image)
-         elif imagenum == 1:
-           canvas1.itemconfigure(self.sprite,image=self.image2)
-         elif imagenum == 2:
-           canvas1.itemconfigure(self.sprite,image=self.image3)
-         elif imagenum == 3:
-           canvas1.itemconfigure(self.sprite,image=self.image4)
-         else: print("Error: add to change image")
+        canvas1.itemconfigure(self.sprite,image=self.imagelist[imagenum])
 
 
 mainwin = Tk()
@@ -44,47 +32,52 @@ mainwin.geometry("1600x900+1+1") # window is at x=1, y=1 on screen (top left)
 canvas1 = Canvas(mainwin,width=1920,height= 1080,bg="black")
 canvas1.place(x=0,y=0)
 
+iconlist = ["sun.png","Showers.png","PartlyCloudy.png","Cloudy.png","Rain.png"]
+iconlistsmall = ["sunsmall.png","Showerssmall.png","PartlyCloudysmall.png","Cloudysmall.png","Rainsmall.png"]
 
 #spritecloud3 = GameObject("Showers.png",x=500,y=30)
-spriteForcast0 = GameObject("sun.png","Showers.png","PartlyCloudy.png","Cloudy.png",x=500,y=30)
-spriteForcast1 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png","Cloudysmall.png",x=350,y=195)
-spriteForcast2 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png","Cloudysmall.png",x=350,y=295)
-spriteForcast3 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png","Cloudysmall.png",x=350,y=395)
-spriteForcast4 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png","Cloudysmall.png",x=350,y=495)
-spriteForcast5 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png","Cloudysmall.png",x=350,y=595)
-spriteForcast6 = GameObject("sunsmall.png","Showerssmall.png","PartlyCloudysmall.png","Cloudysmall.png",x=350,y=695)
+dy = 30
+spriteForcast0 = GameObject(iconlist,x=500,y=30+dy)
+spriteForcast1 = GameObject(iconlistsmall,x=350,y=195+dy)
+spriteForcast2 = GameObject(iconlistsmall,x=350,y=295+dy)
+spriteForcast3 = GameObject(iconlistsmall,x=350,y=395+dy)
+spriteForcast4 = GameObject(iconlistsmall,x=350,y=495+dy)
+spriteForcast5 = GameObject(iconlistsmall,x=350,y=595+dy)
+spriteForcast6 = GameObject(iconlistsmall,x=350,y=695+dy)
 
 fontbig = ("Arial",70)
 fontmedium = ("Arial",45)
 fontsmall = ("Arial",26)
 fonttiny = ("Courier",11)
 fonttiny2 = ("Arial",18)
-mytemptext = canvas1.create_text(300,40,font=fontbig,text="temp",fill="yellow")
-myraintext = canvas1.create_text(800,110,font=fontsmall,text="rain summary",fill="#4343D3")
+mytemptext = canvas1.create_text(300,40+dy,font=fontbig,text="temp",fill="yellow")
+myraintext = canvas1.create_text(800,120+dy,font=fontsmall,text="rain summary",fill="#4343D3")
 myupdatedtext = canvas1.create_text(1400,850,font=fonttiny2,text="updated",fill="yellow")
-day0summary = canvas1.create_text(600,50,font=fontsmall,text="summary",anchor="w",fill="#6B6BEE")
-day1summary = canvas1.create_text(390,200,font=fontsmall,text="summary1",anchor="w",fill="#7373DD")
-day2summary = canvas1.create_text(390,300,font=fontsmall,text="summary2",anchor="w",fill="#7373DD")
-day3summary = canvas1.create_text(390,400,font=fontsmall,text="summary3",anchor="w",fill="#7373DD")
-day4summary = canvas1.create_text(390,500,font=fontsmall,text="summary4",anchor="w",fill="#7373DD")
-day5summary = canvas1.create_text(390,600,font=fontsmall,text="summary4",anchor="w",fill="#7373DD")
-day6summary = canvas1.create_text(390,700,font=fontsmall,text="summary4",anchor="w",fill="#7373DD")
+day0summary = canvas1.create_text(600,50+dy,font=fontsmall,text="summary",anchor="w",fill="#6B6BEE")
+day1summary = canvas1.create_text(390,200+dy,font=fontsmall,text="summary1",anchor="w",fill="#7373DD")
+day2summary = canvas1.create_text(390,300+dy,font=fontsmall,text="summary2",anchor="w",fill="#7373DD")
+day3summary = canvas1.create_text(390,400+dy,font=fontsmall,text="summary3",anchor="w",fill="#7373DD")
+day4summary = canvas1.create_text(390,500+dy,font=fontsmall,text="summary4",anchor="w",fill="#7373DD")
+day5summary = canvas1.create_text(390,600+dy,font=fontsmall,text="summary4",anchor="w",fill="#7373DD")
+day6summary = canvas1.create_text(390,700+dy,font=fontsmall,text="summary4",anchor="w",fill="#7373DD")
 
 daynames = [0]
 for i in range(1,7):
-   daynames.append(canvas1.create_text(20,100+100*i,font=fontsmall,text=f"day{i}",anchor="w",fill="#DDDB73"))
+   daynames.append(canvas1.create_text(20,100+100*i+dy,font=fontsmall,text=f"day{i}",anchor="w",fill="#DDDB73"))
 
 maxtemps = [0]
 for i in range(1,7):
-   maxtemps.append(canvas1.create_text(230,100+100*i,font=fontsmall,text=f"max{i}",anchor="w",fill="#F7F308"))
+   maxtemps.append(canvas1.create_text(230,100+100*i+dy,font=fontsmall,text=f"max{i}",anchor="w",fill="#F7F308"))
 
 daylist = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 
 def chooseicon(myspriteobject, textinfo):
      myspriteobject.changeimage(0) # default to sun
-     if "shower" in  textinfo or "rain" in  textinfo: myspriteobject.changeimage(1)
+     if "shower" in  textinfo: myspriteobject.changeimage(1)
      if "cloudy" in  textinfo: myspriteobject.changeimage(3)
      if "partly cloudy" in  textinfo: myspriteobject.changeimage(2)
+     if "rain" in  textinfo: myspriteobject.changeimage(4)
+     
      
 
 def timer1():
